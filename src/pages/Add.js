46386d49d47f99e50ba9fdef9710eca1df5154b2
Aneesh <aneeshg2001@gmail.com>
2025-07-01 08:48:25 +0530

    pages updated

---
+++ src/pages/Add.js
@@ -0,0 +1,33 @@
+import React, { useState, useContext } from 'react';
+import axios from 'axios';
+import { GlobalContext } from '../context/GlobalContext';
+import MovieCard from '../components/MovieCard';
+
+const Add = () => {
+  const [query, setQuery] = useState('');
+  const [results, setResults] = useState([]);
+  const { watchlist, watched } = useContext(GlobalContext);
+
+  const search = async (e) => {
+    e.preventDefault();
+    const res = await axios.get(`https://www.omdbapi.com/?apikey=YOUR_API_KEY&s=${query}`);
+    setResults(res.data.Search || []);
+  };
+
+  return (
+    <div className="container">
+      <h2>➕ Add Movie</h2>
+      <form onSubmit={search}>
+        <input type="text" placeholder="Search movies..." value={query} onChange={(e) => setQuery(e.target.value)} />
+        <button type="submit">Search</button>
+      </form>
+      <div className="search-results">
+        {results.map((movie) => (
+          <MovieCard key={movie.imdbID} movie={movie} type="watchlist" />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Add;
